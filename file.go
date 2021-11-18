47bd0bba9dd9da2ab0bd99f172acb2cf9864174d
biningo <biningo.cn@gmail.com>
2021-11-18 20:29:51 +0800

    style: remove if

--- file.go
+++ file.go
@@ -35,27 +35,27 @@ func (df *DBFile) Read(offset int64) (e *Entry, err error) {
 	}
 	e = DecodeEntry(buf)
 	offset += HeaderSize
-	if e.KeySize > 0 {
-		key := make([]byte, e.KeySize)
-		if _, err = df.File.ReadAt(key, offset); err != nil {
-			return
-		}
-		e.Key = key
+
+	key := make([]byte, e.KeySize)
+	if _, err = df.File.ReadAt(key, offset); err != nil {
+		return
 	}
+	e.Key = key
+
 	offset += int64(e.KeySize)
-	if e.ValSize > 0 {
-		val := make([]byte, e.ValSize)
-		if _, err = df.File.ReadAt(val, offset); err != nil {
-			return
-		}
-		e.Val = val
+	val := make([]byte, e.ValSize)
+	if _, err = df.File.ReadAt(val, offset); err != nil {
+		return
 	}
+	e.Val = val
 	return
 }
 
-func (df *DBFile) Write(e *Entry) (err error) {
+func (df *DBFile) Write(e *Entry) error {
 	data := e.Encode()
-	_, err = df.File.WriteAt(data, df.Offset)
-	df.Offset += int64(e.Size())
-	return err
+	if _, err := df.File.WriteAt(data, df.Offset); err != nil {
+		return err
+	}
+	df.Offset += e.Size()
+	return nil
 }
